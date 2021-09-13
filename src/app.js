57d9fe4769d4aaf23d5f0a4965dbf157011d9ac9
Nafeu Nasir <nafeu@tenjin.com>
2021-09-13 11:06:05 -0400

    Add additional styles and base app logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { chain } from "lodash";
+import { chain, includes, pull } from "lodash";
 import "./app.css";
 
 import Title from "./components/title";
@@ -9,6 +9,16 @@ import { getMeals } from './services/meals';
 function App() {
   const [meals, setMeals] = useState(getMeals());
   const [shoppingList, setShoppingList] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [isComplete, setIsComplete] = useState(false);
+
+  useEffect(() => {
+    if (cart.length === 0) {
+      setIsComplete(false);
+    } else {
+      setIsComplete(cart.length === shoppingList.length);
+    }
+  }, [shoppingList, cart])
 
   const handleClickMeal = name => {
     const updatedMeals = meals.map(meal => {
@@ -33,26 +43,32 @@ function App() {
     setShoppingList(shoppingList);
   }
 
+  const handleClickItem = item => {
+    if (includes(cart, item)) {
+      setCart(cart.filter(cartItem => cartItem !== item));
+    } else {
+      setCart([...cart, item])
+    }
+  }
+
   return (
     <div className="app">
       <div className="app-container">
         <Title />
-        <div className="shopping-list">
-          {shoppingList.map(item => {
-            return (
-              <div className="shopping-list-item">{item}</div>
-            )
-          })}
-        </div>
         <div className="meals">
           {meals.map(meal => {
             return (
-              <div className={`meal ${meal.selected ? 'selected' : ''}`} onClick={() => handleClickMeal(meal.name)}>
+              <div key={meal.name} className={`meal ${meal.selected ? 'selected' : ''}`} onClick={() => handleClickMeal(meal.name)}>
                 <div className="meal-name">{meal.name}</div>
                 <div className="meal-ingredients">
                   {meal.ingredients.map(ingredient => {
                     return (
-                      <div className="meal-ingredient">{ingredient}</div>
+                      <div
+                        key={ingredient}
+                        className="meal-ingredient"
+                      >
+                        {ingredient}
+                      </div>
                     )
                   })}
                 </div>
@@ -60,6 +76,21 @@ function App() {
             )
           })}
         </div>
+        {shoppingList.length > 0 && (
+          <div className={`shopping-list ${isComplete ? 'is-complete' : ''}`}>
+            {shoppingList.map(item => {
+              return (
+                <div
+                  key={item}
+                  onClick={() => handleClickItem(item)}
+                  className={`shopping-list-item ${includes(cart, item) ? 'in-cart' : 'not-in-cart'}`}
+                >
+                  {item}
+                </div>
+              )
+            })}
+          </div>
+        )}
         <Footer />
       </div>
     </div>
